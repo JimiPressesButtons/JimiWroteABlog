a805afc036273c2309adf739d532190133c63d68
james stitts <jimi@jamess-MacBook-Pro.local>
2016-04-26 10:24:13 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+'use strict';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Backbone = require('backbone');
+
+var main = document.getElementById('main');
+var nav = document.getElementById('nav');
+var footer = document.getElementById('footer');
